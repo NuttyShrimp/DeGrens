4432ba7b10ff543ea5a8417319f04757eeae53de
Pieter-Jan Gevers <pieterjan.gevers@gmail.com>
2021-11-27 14:01:07 +0100

    [MAIN] fix SQL

--- degrens2.sql
+++ degrens2.sql
@@ -1,32 +1,15 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Nov 01, 2021 at 01:41 PM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 7.4.21
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `syndev`
---
-
--- --------------------------------------------------------
 
 CREATE TABLE apartments (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
   `citizenid` VARCHAR(255) NOT NULL UNIQUE,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id`)
 );
 
 --
