243c11fe27a3d9ad92fe5b41379b2d648f5923e1
Jens Galle <galle.jens@telenet.be>
2021-12-19 20:33:51 +0100

    fix(smallresources): better wantedlevel management

--- resources/[qb]/qb-smallresources/client/ignore.lua
+++ resources/[qb]/qb-smallresources/client/ignore.lua
@@ -85,18 +85,11 @@ end)
 
 Citizen.CreateThread(function()
 	while true do
-		local player = PlayerId()
-		for i = 1, 15 do
-			EnableDispatchService(i, false)
-		end
-		if GetPlayerWantedLevel(player) ~= 0 then
-			SetPlayerWantedLevel(player, 0, false)
-			SetPlayerWantedLevelNow(player, false)
-			SetPlayerWantedLevelNoDrop(player, 0, false)
-		else
-			Citizen.Wait(500)
-		end
-		Citizen.Wait(6)
+        for i = 1, 15 do
+            EnableDispatchService(i, false)
+        end
+    
+        SetMaxWantedLevel(0)
 	end
 end)
 
