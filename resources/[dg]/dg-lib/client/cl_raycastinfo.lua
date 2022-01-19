4f7bf52eff09ce76044e7c92e6de8e47fcc20ae5
NuttyShrimp <jan.lecoutere@hotmail.be>
2022-01-20 00:25:20 +0100

    refactor(lib/raycast): replaced math on table by doing math on param

--- resources/[dg]/dg-lib/client/cl_raycastinfo.lua
+++ resources/[dg]/dg-lib/client/cl_raycastinfo.lua
@@ -1,48 +1,57 @@
-function RotationToDirection(rotation)
-	local adjustedRotation = {
-		x = (math.pi / 180) * rotation.x,
-		y = (math.pi / 180) * rotation.y,
-		z = (math.pi / 180) * rotation.z
-	}
-	local direction = {
-		x = -math.sin(adjustedRotation.z) * math.abs(math.cos(adjustedRotation.x)),
-		y = math.cos(adjustedRotation.z) * math.abs(math.cos(adjustedRotation.x)),
-		z = math.sin(adjustedRotation.x)
-	}
-	return direction
+local isInVehicle = false
+local debugEnabled = false
+originCoords = nil
+forwardCoords = nil
+
+function getForwardVector(rotation)
+	local rot = (math.pi / 180) * rotation
+	return vector3(-math.sin(rot.z) * math.abs(math.cos(rot.x)), math.cos(rot.z) * math.abs(math.cos(rot.x)), math.sin(rot.x))
 end
 
-function RayCastGamePlayCamera(distance)
-	local cameraRotation = GetGameplayCamRot()
-	local cameraCoord = GetGameplayCamCoord()
-	local direction = RotationToDirection(cameraRotation)
-	local destination = {
-		x = cameraCoord.x + direction.x * distance,
-		y = cameraCoord.y + direction.y * distance,
-		z = cameraCoord.z + direction.z * distance
-	}
-	local retval, hit, endCoords, surfaceNormal, entity = GetShapeTestResult(StartShapeTestSweptSphere(cameraCoord.x, cameraCoord.y, cameraCoord.z, destination.x, destination.y, destination.z, 0.3, 286, PlayerPedId(), 0))
-	return hit, endCoords, entity
+function rayCast(origin, target, radius, flags, ignore)
+	local handle = StartShapeTestSweptSphere(origin.x, origin.y, origin.z, target.x, target.y, target.z, radius, flags, ignore, 0)
+	return GetShapeTestResult(handle)
+end
+
+function getEntityPlayerLookingAt(pDistance, pRadius, pFlag, pIgnore)
+	pDistance = pDistance or 3.0
+	originCoords = GetPedBoneCoords(PlayerPedId(), 31086)
+	local forwardVector = getForwardVector(GetGameplayCamRot(2))
+	forwardCoords = originCoords + forwardVector * (isInVehicle and pDistance + 1.5 or pDistance)
+
+	if not forwardVector then
+		return
+	end
+
+	local _, hit, coords, _, entity = rayCast(originCoords, forwardCoords, pRadius, pFlag, pIgnore)
+
+	if not hit and entity == 0 then
+		return
+	end
+
+	local entityType = GetEntityType(entity)
+
+	return entity, entityType, coords
 end
 
 Citizen.CreateThread(function()
 	while true do
-		local sleep = 250
+		local ped = PlayerPedId()
+		local entity, entityType, entityCoords = getEntityPlayerLookingAt(3.0, 0.2, 286, ped)
 
-		local hit, targetCoords, targetEntity = RayCastGamePlayCamera(100.0)
-
-		local entityType = GetEntityType(targetEntity)
-		if hit and targetEntity ~= 0 and entityType ~= 0 then
-			if targetEntity ~= CurrentTarget then
-				CurrentTarget = targetEntity
-				TriggerEvent('dg-lib:targetinfo:changed', CurrentTarget, entityType, targetCoords)
+		if entity and entityType ~= 0 then
+			if entity ~= CurrentTarget then
+				CurrentTarget = entity
+				TriggerEvent('dg-lib:targetinfo:changed', CurrentTarget, entityType, entityCoords)
+				debug('[raycastinfo] Target changed to ' .. tostring(entity) .. ' (' .. tostring(entityType) .. ')')
 			end
 		elseif CurrentTarget then
 			CurrentTarget = nil
 			TriggerEvent('dg-lib:targetinfo:changed', CurrentTarget)
+			debug('[raycastinfo] Target changed to nothing')
 		end
 
-		Citizen.Wait(sleep)
+		Citizen.Wait(250)
 	end
 end)
 
@@ -56,7 +65,22 @@ exports('GetCurrentEntity', function(distance)
 	return 0
 end)
 
-exports('GetEntityInFrontOfPlayer', function()
-	local hit, _, targetEntity = RayCastGamePlayCamera(100.0)
-	return targetEntity
+AddEventHandler('baseevents:enteredVehicle', function()
+	isInVehicle = true
 end)
+
+AddEventHandler('baseevents:leftVehicle', function()
+	isInVehicle = false
+end)
+
+if GetConvar('is_production', 'true') == 'false' then
+	RegisterCommand('lib:raycast', function()
+		debugEnabled = true
+		CreateThread(function()
+			while debugEnabled do
+				DrawLine(originCoords.x, originCoords.y, originCoords.z, forwardCoords.x, forwardCoords.y, forwardCoords.z, 255, 0, 0, 255)
+				Wait(0)
+			end
+		end)
+	end)
+end
