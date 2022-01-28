8d6548c580339f9120137ded46056f34780d4d62
NuttyShrimp <jan.lecoutere@hotmail.be>
2022-01-28 19:08:32 +0100

    chore(lerna: added eslint to lerna and fixed errors where needed

--- resources/[dg]/dg-financials/typescript/src/client/util.ts
+++ resources/[dg]/dg-financials/typescript/src/client/util.ts
@@ -1,6 +1,8 @@
-export const RegisterUICallback = (name: string, handler: (data: any) => any): void => {
+type dataFunction = (_data: any) => any;
+
+export const RegisterUICallback = (name: string, handler: dataFunction): void => {
 	RegisterNuiCallbackType(name);
-	on(`__cfx_nui:${name}`, async (data: any, cb: (...args: any) => void) => {
+	on(`__cfx_nui:${name}`, async (data: any, cb: (..._args: any) => void) => {
 		const result = await handler(data);
 		cb({ data: result ?? {}, meta: { ok: true, message: 'done' } });
 	});
