fa306d3695b45e5041ffccc308304246e379b0e7
Jens Galle <galle.jens@telenet.be>
2022-03-13 02:36:29 +0100

    feat(blackmoney): add small script for randomselling blackmoney

---
+++ resources/[dg]/dg-blackmoney/config.lua
@@ -0,0 +1,22 @@
+Config = {}
+
+Config.Items = {
+    'moneyroll',
+    'markedbills'
+}
+
+Config.Worth = {
+    ['moneyroll'] = {
+        min = 75,
+        max = 150
+    },
+    ['markedbills'] = {
+        min = 250,
+        max = 400,
+    }
+}
+
+Config.RandomSellAmount = {
+    min = 1, 
+    max = 5,
+}
