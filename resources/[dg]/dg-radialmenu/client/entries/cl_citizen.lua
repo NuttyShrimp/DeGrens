c5b5ed34c08ec3eadd15c28d7809151437a49e98
NuttyShrimp <jan.lecoutere@hotmail.be>
2022-03-25 01:25:06 +0000

    refactor(radialmenu): refactored entries logic

---
+++ resources/[dg]/dg-radialmenu/client/entries/cl_citizen.lua
@@ -0,0 +1,53 @@
+entries.citizen = {
+	{
+		id = 'givenum',
+		title = 'Geef telefoonnummer',
+		icon = 'address-book',
+		type = 'server',
+		event = 'dg-phone:server:contacts:shareNumber',
+		shouldClose = true,
+		isEnabled = function()
+			return DGCore.Functions.TriggerCallback('DGCore:HasItem', 'phone')
+		end
+	},
+	{
+		id = 'getintrunk',
+		title = 'Stap in koffer',
+		icon = 'car',
+		event = 'qb-trunk:client:GetIn',
+		shouldClose = true,
+		isEnabled = function()
+			-- TODO: Check if near vehicle
+			return true
+		end
+	},
+	{
+		id = 'cornerselling',
+		title = 'Corner Selling',
+		icon = 'cannabis',
+		event = 'dg-cornerselling:client:ToggleSelling',
+		shouldClose = true,
+		isEnabled = function()
+			return true
+		end
+	},
+	{
+		id = 'togglehotdogsell',
+		title = 'Hotdog Selling',
+		icon = 'hotdog',
+		event = 'qb-hotdogjob:client:ToggleSell',
+		shouldClose = true,
+		isEnabled = function(playerData)
+			return playerData.job.name == 'hotdog'
+		end
+	},
+	{
+		id = 'interactions',
+		title = 'Interacties',
+		icon = 'exclamation-triangle',
+		subMenu = 'interactions',
+		isEnabled = function()
+			return true
+		end
+	},
+}
---
+++ resources/[dg]/dg-radialmenu/client/entries/cl_vehicledoors.lua
@@ -0,0 +1,62 @@
+entries.vehicledoors = {
+	{
+		id = 'door0',
+		title = 'Drivers door',
+		icon = 'car-side',
+		event = 'qb-radialmenu:client:openDoor',
+		shouldClose = false,
+		isEnabled = function()
+			return true
+		end
+	},
+	{
+		id = 'door4',
+		title = 'Hood',
+		icon = 'car',
+		event = 'qb-radialmenu:client:openDoor',
+		shouldClose = false,
+		isEnabled = function()
+			return true
+		end
+	},
+{
+		id = 'door1',
+		title = 'Passengers door',
+		icon = 'car-side',
+		event = 'qb-radialmenu:client:openDoor',
+		shouldClose = false,
+		isEnabled = function()
+			return true
+		end
+	},
+	{
+		id = 'door3',
+		title = 'Right rear',
+		icon = 'car-side',
+		event = 'qb-radialmenu:client:openDoor',
+		shouldClose = false,
+		isEnabled = function()
+			return true
+		end
+	},
+{
+		id = 'door5',
+		title = 'Trunk',
+		icon = 'car',
+		event = 'qb-radialmenu:client:openDoor',
+		shouldClose = false,
+		isEnabled = function()
+			return true
+		end
+	},
+	{
+		id = 'door2',
+		title = 'Left rear',
+		icon = 'car-side',
+		event = 'qb-radialmenu:client:openDoor',
+		shouldClose = false,
+		isEnabled = function()
+			return true
+		end
+	}
+}
