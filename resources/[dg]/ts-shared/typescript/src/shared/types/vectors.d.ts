bf5761444a2208936fedc4b93f1fb51db111415f
NuttyShrimp <jan.lecoutere@hotmail.be>
2022-04-14 22:44:31 +0200

    feat(dgx): added vec2 vector interface

--- resources/[dg]/ts-shared/typescript/src/shared/types/vectors.d.ts
+++ resources/[dg]/ts-shared/typescript/src/shared/types/vectors.d.ts
@@ -1,12 +1,12 @@
-declare interface Vec3 {
+declare interface Vec2 {
   x: number;
   y: number;
+}
+
+declare interface Vec3 extends Vec2 {
   z: number;
 }
 
-declare interface Vec4 {
-  x: number;
-  y: number;
-  z: number;
+declare interface Vec4 extends Vec3 {
   w: number;
 }
