3304f0bcf6649b84a9e5b145618ce5af45f7242c
NuttyShrimp <jan.lecoutere@hotmail.be>
2022-04-21 22:16:09 +0200

    fix(dgx): added metatable to fix null refs on script start

--- resources/[dg]/ts-shared/shared/lib.lua
+++ resources/[dg]/ts-shared/shared/lib.lua
@@ -1,6 +1,26 @@
 --- Use this lib file to use the DGX library in lua scripts.
 DGX = nil
 
+local function getProxy(proxyKey)
+  return setmetatable({}, {
+    __call = function(self, ...)
+      args = { ... }
+      -- If the lib is initialized, we save it in another var
+      lib = DGX
+      -- split by | and loop with gmatch
+      for k, v in string.gmatch(proxyKey, "([^|]+)") do
+        -- TODO: Find way to see if the key exists
+        lib = lib[k]
+      end
+    end,
+    __index = function(t, k)
+      return getProxy(proxyKey ~= nil and proxyKey .. '|' .. k or k)
+    end
+  })
+end
+
+DGX = getProxy()
+
 Citizen.CreateThread(function()
   DGX = exports[GetCurrentResourceName()]:_getLibrary()
 end)
