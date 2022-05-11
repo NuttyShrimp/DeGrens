145af5e8b04628c5f2eab0f21651cf78785c06a6
Jens Galle <galle.jens@telenet.be>
2022-05-11 21:37:48 +0200

    style(ui): fix prettier problems that caused build errors

--- resources/[dg]/dg-ui/[src]/src/index.provider.tsx
+++ resources/[dg]/dg-ui/[src]/src/index.provider.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import * as Sentry from '@sentry/react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Alert, Snackbar } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import { StyledEngineProvider, ThemeProvider } from '@mui/material/styles';
+import * as Sentry from '@sentry/react';
 
 import { nuiAction } from './lib/nui-comms';
 import { GetInitialState, type } from './lib/redux';
@@ -23,30 +23,30 @@ export const IndexProvider = ({ children }) => {
 
   return (
     <Sentry.ErrorBoundary fallback={<div>An error happenend in the root of UI, Restart the ui</div>} showDialog>
-          <StyledEngineProvider injectFirst>
-              <ThemeProvider theme={theme}>
-                  <CssBaseline />
-                  {mainState.mounted ? (
-                      children
-                  ) : (
-                      <Snackbar
-                          open={true}
-                          autoHideDuration={3000}
-                          anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-                          onClose={handleClose}
-                      >
-                          <Alert
-                              onClose={handleClose}
-                              variant='filled'
-                              severity={mainState.error ? 'error' : 'info'}
-                              sx={{ width: '100%' }}
-                          >
-                              {mainState.error ? `An error occurred in ${mainState.error}.` : ''} Reloading the UI...
-                          </Alert>
-                      </Snackbar>
-                  )}
-              </ThemeProvider>
-          </StyledEngineProvider>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {mainState.mounted ? (
+            children
+          ) : (
+            <Snackbar
+              open={true}
+              autoHideDuration={3000}
+              anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+              onClose={handleClose}
+            >
+              <Alert
+                onClose={handleClose}
+                variant='filled'
+                severity={mainState.error ? 'error' : 'info'}
+                sx={{ width: '100%' }}
+              >
+                {mainState.error ? `An error occurred in ${mainState.error}.` : ''} Reloading the UI...
+              </Alert>
+            </Snackbar>
+          )}
+        </ThemeProvider>
+      </StyledEngineProvider>
     </Sentry.ErrorBoundary>
   );
 };
