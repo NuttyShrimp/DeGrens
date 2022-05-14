76d83a71be9f7b5a918a1726bcfe04b1b37058af
NuttyShrimp <jan.lecoutere@hotmail.be>
2022-05-14 22:56:28 +0200

    fix(dgx): added missing logic to proxy function for DGX

--- resources/[dg]/ts-shared/shared/lib.lua
+++ resources/[dg]/ts-shared/shared/lib.lua
@@ -4,14 +4,24 @@ DGX = nil
 local function getProxy(proxyKey)
   return setmetatable({}, {
     __call = function(self, ...)
-      args = { ... }
+      local args = { ... }
       -- If the lib is initialized, we save it in another var
-      lib = DGX
-      -- split by | and loop with gmatch
-      for k, v in string.gmatch(proxyKey, "([^|]+)") do
-        -- TODO: Find way to see if the key exists
-        lib = lib[k]
-      end
+      local p = promise:new()
+      
+      Citizen.CreateThread(function()
+        local lib = DGX
+        -- split by | and loop with gmatch
+        for k, v in string.gmatch(proxyKey, "([^|]+)") do
+          -- TODO: Find way to see if the key exists
+          lib = lib[k]
+        end
+        
+        local result = lib(table.unpack(args))
+
+        p:resolve(result)
+      end)
+      
+      return p
     end,
     __index = function(t, k)
       return getProxy(proxyKey ~= nil and proxyKey .. '|' .. k or k)
