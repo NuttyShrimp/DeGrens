5c33b1d2f2980391289c0ff8cca1016a29d174c7
NuttyShrimp <jan.lecoutere@hotmail.be>
2022-06-05 23:02:19 +0200

    fix(financials): added missing label for blip

--- resources/[dg]/dg-financials/typescript/src/client/classes/Location.ts
+++ resources/[dg]/dg-financials/typescript/src/client/classes/Location.ts
@@ -59,6 +59,9 @@ export class Location {
     SetBlipDisplay(this.blip, 2);
     SetBlipScale(this.blip, 0.8);
     SetBlipAsShortRange(this.blip, true);
+    BeginTextCommandScaleformString("string");
+    AddTextComponentString("Bank");
+    EndTextCommandScaleformString();
   }
 
   //endregion
