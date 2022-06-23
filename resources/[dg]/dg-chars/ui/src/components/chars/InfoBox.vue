5dac0048a527a504f3dff59e1d32f0ff4a11b1b3
NuttyShrimp <jan.lecoutere@hotmail.be>
2022-06-23 13:28:08 +0200

    fix(chars/ui): added inputvalidation to prevent empty or really short names

--- resources/[dg]/dg-chars/ui/src/components/chars/InfoBox.vue
+++ resources/[dg]/dg-chars/ui/src/components/chars/InfoBox.vue
@@ -39,9 +39,8 @@
     <q-dialog v-model="deleteDialog" persistent>
       <q-card>
         <q-card-section class="row items-center">
-          <span class="q-ml-sm"
-            >Weet je zeker dat je je karakter wilt verwijderen? Dit kan niet ongedaan gemaakt worden!</span
-          >
+          <span class="q-ml-sm">Weet je zeker dat je je karakter wilt verwijderen? Dit kan niet ongedaan gemaakt
+            worden!</span>
         </q-card-section>
 
         <q-card-actions align="right">
@@ -57,9 +56,10 @@
         </q-card-section>
 
         <q-card-section class="q-pt-none">
-          <q-input v-model="charInfo.firstname" dense label="Voornaam" autofocus />
-          <q-input v-model="charInfo.lastname" dense label="Achternaam" autofocus />
-          <q-input v-model="charInfo.nationality" dense label="Nationaliteit" autofocus />
+          <q-input v-model="charInfo.firstname" dense label="Voornaam" autofocus :rules="validationRules" />
+          <q-input v-model="charInfo.lastname" dense label="Achternaam" autofocus :rules="validationRules" />
+          <q-input v-model="charInfo.nationality" dense label="Nationaliteit" autofocus
+            :rules="[val => !!val || 'Field is required']" />
           <div class="q-gutter-sm">
             <q-radio v-model="charInfo.gender" :val="0" label="Man" />
             <q-radio v-model="charInfo.gender" :val="1" label="Vrouw" />
@@ -89,81 +89,88 @@
 </template>
 
 <script lang="ts">
-  import { computed, defineComponent, reactive, ref } from 'vue';
+import { computed, defineComponent, reactive, ref } from 'vue';
 
-  import { nuiAction } from '../../lib/nui/action';
-  import { useStore } from '../../lib/store';
-
-  export default defineComponent({
-    name: 'InfoBox',
-    props: {
-      loc: {
-        type: Object,
-        default: () => ({ top: 0, left: 0 }),
-      },
+import { nuiAction } from '../../lib/nui/action';
+import { useStore } from '../../lib/store';
+const fillWithZero = (num: number) => {
+  if (num <= 9) return "0" + num;
+  return num
+}
+export default defineComponent({
+  name: 'InfoBox',
+  props: {
+    loc: {
+      type: Object,
+      default: () => ({ top: 0, left: 0 }),
     },
-    setup() {
-      const today18Date = new Date(Date.now() - 18 * 365.25 * 24 * 60 * 60 * 1000);
-      const today18 = `${today18Date.getFullYear()}/${
-        today18Date.getMonth() + 1 < 10 ? '0' + (today18Date.getMonth() + 1) : today18Date.getMonth() + 1
-      }/${today18Date.getDate()}`;
-      const store = useStore();
-      const currentCharacter = computed(() => store.state.currentCharacter);
-      const isPosFrozen = computed(() => store.state.freezePosition);
-      const deleteDialog = ref(false);
-      const createDialog = ref(false);
-      const charInfo = reactive({
-        firstname: '',
-        lastname: '',
-        nationality: 'Belg',
-        birthdate: today18,
-        gender: 0,
-      });
-      const dobLimit = (date: string) => {
-        // Smaller or eq to today18
-        return date <= today18;
-      };
+  },
+  setup() {
+    const today18Date = new Date(Date.now() - 18 * 365.25 * 24 * 60 * 60 * 1000);
+    const today18 =
+      `${today18Date.getFullYear()}/${fillWithZero(today18Date.getMonth() + 1)}/${fillWithZero(today18Date.getDate())}`;
+    const store = useStore();
+    const currentCharacter = computed(() => store.state.currentCharacter);
+    const isPosFrozen = computed(() => store.state.freezePosition);
+    const deleteDialog = ref(false);
+    const createDialog = ref(false);
+    const charInfo = reactive({
+      firstname: '',
+      lastname: '',
+      nationality: 'Belg',
+      birthdate: today18,
+      gender: 0,
+    });
 
-      const spawn = () => {
-        nuiAction('selectChar');
-      };
+    const dobLimit = (date: string) => {
+      // Smaller or eq to today18
+      return date <= today18;
+    };
 
-      const deleteChar = () => {
-        deleteDialog.value = true;
-      };
+    const spawn = () => {
+      nuiAction('selectChar');
+    };
 
-      const confirmDelete = () => {
-        deleteDialog.value = false;
-        nuiAction('deleteChar');
-      };
+    const deleteChar = () => {
+      deleteDialog.value = true;
+    };
 
-      const createChar = () => {
-        createDialog.value = true;
-      };
+    const confirmDelete = () => {
+      deleteDialog.value = false;
+      nuiAction('deleteChar');
+    };
 
-      const confirmCreate = () => {
-        createDialog.value = false;
-        nuiAction('createChar', charInfo);
-      };
+    const createChar = () => {
+      createDialog.value = true;
+    };
 
-      const unFreeze = () => {
-        store.commit('setFreezePosition', false);
-      };
+    const confirmCreate = () => {
+      createDialog.value = false;
+      nuiAction('createChar', charInfo);
+    };
 
-      return {
-        currentCharacter,
-        isPosFrozen,
-        spawn,
-        deleteChar,
-        createChar,
-        confirmDelete,
-        confirmCreate,
-        unFreeze,
-        deleteDialog,
-        createDialog,
-        charInfo,
-        dobLimit,
-      };
-    },
-  });
+    const unFreeze = () => {
+      store.commit('setFreezePosition', false);
+    };
+
+    return {
+      currentCharacter,
+      isPosFrozen,
+      spawn,
+      deleteChar,
+      createChar,
+      confirmDelete,
+      confirmCreate,
+      unFreeze,
+      deleteDialog,
+      createDialog,
+      charInfo,
+      dobLimit,
+      validationRules: [
+        (val: string) => !!val || '* Verplicht',
+        (val: string) => val.length > 2 || 'Je naam moet minstens 3 karakters lang zijn',
+      ]
+    };
+  },
+});
 </script>
