7913d920207252b923107196f4adcee22ed2a172
NuttyShrimp <jan.lecoutere@hotmail.be>
2022-08-20 18:39:22 +0200

    style(notifications): restyled to more visible variant

--- resources/[dg]/dg-ui/[src]/src/main/notifications/components/list.tsx
+++ resources/[dg]/dg-ui/[src]/src/main/notifications/components/list.tsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import { animated, easings, Transition } from 'react-spring';
-import { useVhToPixel } from '@lib/hooks/useVhToPixel';
-
-import { Notification } from './notification';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import ErrorIcon from '@mui/icons-material/Error';
+import InfoIcon from '@mui/icons-material/Info';
 
 export const NotificationList: AppFunction<Notifications.State> = props => {
-  const hiddenMargin = useVhToPixel(-37);
-  const showMargin = useVhToPixel(1);
   return (
     <div className={'notification__list'}>
       <Transition
         items={props.notifications}
         config={{
-          duration: 500,
+          duration: 300,
           easing: easings.easeInOutQuart,
         }}
-        from={{ marginRight: hiddenMargin }}
-        enter={{ marginRight: showMargin }}
-        leave={{ marginRight: hiddenMargin }}
+        from={{ opacity: 0 }}
+        enter={{ opacity: 1 }}
+        leave={{ opacity: 0 }}
       >
         {(style, item) => (
           <animated.div style={style}>
-            <Notification {...item} />
+            <div className="notification__outer">
+              <div className={`notification ${item.type}`}>
+                {item.type === 'info' && <InfoIcon />}
+                {item.type === 'error' && <ErrorIcon />}
+                {item.type === 'success' && <CheckCircleIcon />}
+                <p>{item.message}</p>
+              </div>
+            </div>
           </animated.div>
         )}
       </Transition>
