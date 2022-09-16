86fa5c092bce24134f9682769af019b33a6077f5
Jens Galle <galle.jens@telenet.be>
2022-09-16 18:39:24 +0200

    fix(taskbar): centered taskbar icon

--- resources/[dg]/dg-ui/[src]/src/main/taskbar/component/TaskBar.tsx
+++ resources/[dg]/dg-ui/[src]/src/main/taskbar/component/TaskBar.tsx
@@ -5,7 +5,9 @@ export const TaskBar: FC<TaskBar.State> = props => {
   return (
     <div className={'taskbar__wrapper'}>
       <div className={'taskbar__innerwrapper'}>
-        <FillableIcon height={7} name={props.icon} duration={props.duration} value={100} />
+        <div className='taskbar__icon'>
+          <FillableIcon height={7} name={props.icon} duration={props.duration} value={100} />
+        </div>
         <div>
           <div className={'taskbar__label'}>{props.label}</div>
         </div>
