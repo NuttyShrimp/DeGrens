ccb4f05d633ffa8ca1f5587555dbf7e7d7a3ca65
NuttyShrimp <jan.lecoutere@hotmail.be>
2022-09-21 16:44:14 +0200

    fix(asyncExports): fixed empty function error

--- resources/[dg]/ts-shared/typescript/src/shared/helpers/asyncExports.ts
+++ resources/[dg]/ts-shared/typescript/src/shared/helpers/asyncExports.ts
@@ -1,16 +1,21 @@
 const cfxExports = global.exports;
 
-global.asyncExports = new Proxy(() => {}, {
-  apply(t, self, args) {
-    if (args.length !== 2) {
-      throw new Error('this needs 2 arguments');
-    }
-
-    const [exportName, func] = args;
-    const middlewareFunc = async (...args: any[]) => {
-      await new Promise(res => setImmediate(res));
-      return func(...args);
-    };
-    cfxExports(exportName, middlewareFunc);
+global.asyncExports = new Proxy(
+  () => {
+    // empty
   },
-});
+  {
+    apply(t, self, args) {
+      if (args.length !== 2) {
+        throw new Error('this needs 2 arguments');
+      }
+
+      const [exportName, func] = args;
+      const middlewareFunc = async (...args: any[]) => {
+        await new Promise(res => setImmediate(res));
+        return func(...args);
+      };
+      cfxExports(exportName, middlewareFunc);
+    },
+  }
+);
