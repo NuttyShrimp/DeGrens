19eac2eec3a3fa2d3c75857138448ff19e5f7a2e
Jens Galle <galle.jens@telenet.be>
2022-09-29 21:03:34 +0200

    refactor(core): optimized get closest veh function

--- resources/[core]/dg-core/client/functions.lua
+++ resources/[core]/dg-core/client/functions.lua
@@ -212,25 +212,25 @@ function DGCore.Functions.GetPlayersFromCoords(coords, distance, serverIds)
 end
 
 function DGCore.Functions.GetClosestVehicle(coords)
-    local ped = PlayerPedId()
-    local vehicles = GetGamePool('CVehicle')
-    local closestDistance = -1
-    local closestVehicle = -1
-    if coords then
-        coords = type(coords) == 'table' and vec3(coords.x, coords.y, coords.z) or coords
-    else
-        coords = GetEntityCoords(ped)
-    end
-    for i = 1, #vehicles, 1 do
-        local vehicleCoords = GetEntityCoords(vehicles[i])
-        local distance = #(vehicleCoords - coords)
+  if coords then
+    coords = type(coords) == 'table' and vec3(coords.x, coords.y, coords.z) or coords
+  else
+    coords = GetEntityCoords(PlayerPedId())
+  end
 
-        if closestDistance == -1 or closestDistance > distance then
-            closestVehicle = vehicles[i]
-            closestDistance = distance
-        end
+  local vehicles = GetGamePool('CVehicle')
+  local closestDistance = 99999999
+  local closestVehicle = nil
+
+  for _, vehicle in pairs(vehicles) do
+    local distance = #(GetEntityCoords(vehicle) - coords)
+    if closestDistance > distance then
+      closestVehicle = vehicle
+      closestDistance = distance
     end
-    return closestVehicle, closestDistance
+  end
+
+  return closestVehicle, closestDistance
 end
 
 function DGCore.Functions.GetClosestObject(coords)
