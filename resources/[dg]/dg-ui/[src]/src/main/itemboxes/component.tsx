03a53f63e7e8da86549dd19f2880eca13b0c1060
Jens Galle <galle.jens@telenet.be>
2022-10-02 00:15:32 +0200

    refactor(ui): use updatestate callback to remove dependency

--- resources/[dg]/dg-ui/[src]/src/main/itemboxes/component.tsx
+++ resources/[dg]/dg-ui/[src]/src/main/itemboxes/component.tsx
@@ -11,19 +11,22 @@ const Component: AppFunction<Itemboxes.State> = props => {
     props.updateState({ visible });
   };
 
-  const eventHandler = useCallback(
-    (data: any) => {
-      const itembox: Itemboxes.Itembox = {
-        action: data.action ?? '',
-        image: data.image ?? 'noicon.png',
-      };
-      props.updateState({ itemboxes: [itembox, ...props.itemboxes] });
-      setTimeout(() => {
-        props.updateState({ itemboxes: props.itemboxes.filter((_, i) => i !== props.itemboxes.length - 1) });
-      }, 3000);
-    },
-    [props.itemboxes]
-  );
+  const eventHandler = useCallback((data: any) => {
+    const itembox: Itemboxes.Itembox = {
+      action: data.action ?? '',
+      image: data.image ?? 'noicon.png',
+    };
+    props.updateState(rootState => ({ itemboxes: [itembox, ...rootState.itemboxes.itemboxes] }));
+    // We can just remove last item because new ones get added to front of array and removal time is always same so last one is always oldest
+    setTimeout(() => {
+      props.updateState(rootState => {
+        const lastItemIndex = rootState.itemboxes.itemboxes.length - 1;
+        return {
+          itemboxes: rootState.itemboxes.itemboxes.filter((_, i) => i !== lastItemIndex),
+        };
+      });
+    }, 3000);
+  }, []);
 
   const handleShow = useCallback(() => handleVisibility(true), []);
   const handleHide = useCallback(() => handleVisibility(false), []);
