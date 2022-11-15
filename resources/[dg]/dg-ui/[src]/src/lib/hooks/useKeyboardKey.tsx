70009435f5ae87439e40cdf511774dbcbeb5d9ab
NuttyShrimp <jan.lecoutere@hotmail.be>
2022-11-15 08:06:55 +0000

    feat(ui): add kbd layout to configmenu + hook to translate from qwerty to azerty

---
+++ resources/[dg]/dg-ui/[src]/src/lib/hooks/useKeyboardKey.tsx
@@ -0,0 +1,90 @@
+import { MutableRefObject, useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
+
+const QWERTY_TO_AZERTY = {
+  '`': '²',
+  '1': '&',
+  '!': '1',
+  '2': 'é',
+  '@': '2',
+  '3': '"',
+  '#': '3',
+  '4': "'",
+  $: '4',
+  '5': '(',
+  '%': '5',
+  '6': '-',
+  '^': '6',
+  '7': 'è',
+  '&': '7',
+  '8': '_',
+  '*': '8',
+  '9': 'ç',
+  '(': '9',
+  '0': 'à',
+  ')': '0',
+  '-': ')',
+  _: '°',
+  '=': '=',
+  '+': '+',
+  q: 'a',
+  w: 'z',
+  e: 'e',
+  r: 'r',
+  t: 't',
+  y: 'y',
+  u: 'u',
+  i: 'i',
+  o: 'o',
+  p: 'p',
+  '[': '^',
+  '{': '¨',
+  ']': '$',
+  '}': '£',
+  '\\': '*',
+  '|': 'µ',
+  a: 'q',
+  s: 's',
+  d: 'd',
+  f: 'f',
+  g: 'g',
+  h: 'h',
+  j: 'j',
+  k: 'k',
+  l: 'l',
+  ';': 'm',
+  ':': 'M',
+  "'": 'ù',
+  '"': '%',
+  z: 'w',
+  x: 'x',
+  c: 'c',
+  v: 'v',
+  b: 'b',
+  n: 'n',
+  m: ',',
+  M: '?',
+  '.': ':',
+  '<': '.',
+  '>': '/',
+  '/': '!',
+  '?': '§',
+  ',': ';',
+  ' ': ' ',
+  // TODO: add numbers
+};
+
+export const useKeyboardKey = (key: string) => {
+  const layout = useSelector<RootState, string>(s => s.configmenu.hud.keyboard);
+  const keyRef: MutableRefObject<string> = useRef(key);
+
+  useEffect(() => {
+    if (QWERTY_TO_AZERTY[key.toLowerCase()] && layout == 'azerty') {
+      keyRef.current = QWERTY_TO_AZERTY[key.toLowerCase()];
+    } else {
+      keyRef.current = key;
+    }
+  }, [layout]);
+
+  return { key: keyRef.current ?? key };
+};
--- resources/[dg]/dg-ui/[src]/src/main/bennys/hooks/useKeyEvents.tsx
+++ resources/[dg]/dg-ui/[src]/src/main/bennys/hooks/useKeyEvents.tsx
@@ -1,4 +1,5 @@
 import { MutableRefObject, useEffect, useRef } from 'react';
+import { useKeyboardKey } from '@src/lib/hooks/useKeyboardKey';
 
 // https://developer.mozilla.org/en-US/docs/Web/API/UI_Events/Keyboard_event_key_values
 declare type Key = 'ArrowUp' | 'ArrowDown' | 'ArrowLeft' | 'ArrowRight' | 'q' | 'e' | ' ' | 'Escape' | 'Enter' | 'Tab';
@@ -6,6 +7,7 @@ declare type Key = 'ArrowUp' | 'ArrowDown' | 'ArrowLeft' | 'ArrowRight' | 'q' | 
 export const useKeyEvents = () => {
   const useEventRegister = (key: Key, handler: () => void) => {
     const handlerRef: MutableRefObject<(() => void) | undefined> = useRef();
+    const { key: translatedKey } = useKeyboardKey(key);
 
     useEffect(() => {
       handlerRef.current = handler;
@@ -13,7 +15,7 @@ export const useKeyEvents = () => {
 
     useEffect(() => {
       const internalHandler = (e: KeyboardEvent) => {
-        if (e.key === key) {
+        if (e.key === translatedKey) {
           if (handlerRef.current) {
             handlerRef.current();
           }
@@ -23,7 +25,7 @@ export const useKeyEvents = () => {
       return () => {
         window.removeEventListener('keydown', internalHandler);
       };
-    }, [key]);
+    }, [translatedKey]);
   };
   return {
     useEventRegister,
