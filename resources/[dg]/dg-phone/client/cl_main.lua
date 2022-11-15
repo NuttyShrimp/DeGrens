7f64c3e5e26177bd7095b65bd5d0f3a1e369d0bf
NuttyShrimp <jan.lecoutere@hotmail.be>
2022-11-15 01:10:30 +0100

    fix(phone): fix phone not hiding with camera + also close phone from client

--- resources/[dg]/dg-phone/client/cl_main.lua
+++ resources/[dg]/dg-phone/client/cl_main.lua
@@ -1,32 +1,34 @@
 openPhone = function()
-	if not canOpen() then
-		return
-	end
-	openApplication('phone', nil, true)
-	SetUIFocusCustom(true, true)
+  if not canOpen() then
+    return
+  end
+  openApplication('phone', nil, true)
+  SetUIFocusCustom(true, true)
   exports['dg-lib']:shouldExecuteKeyMaps(false)
-	setState('state', 1)
+  setState('state', 1)
 end
 
-closePhone = function(state)
-	if getState('state') == 2 then
-		closeCamera()
-	end
--- 	closeApplication('phone')
-	disablePauseMenu()
-	SetUIFocusCustom(false, false)
-	setState('state', state or 0)
+closePhone = function(state, skipUI)
+  if getState('state') == 2 then
+    closeCamera()
+  end
+  if not skipUI then
+    closeApplication('phone')
+  end
+  disablePauseMenu()
+  SetUIFocusCustom(false, false)
+  setState('state', state or 0)
   exports['dg-lib']:shouldExecuteKeyMaps(state and state == 0 or true)
 end
 
 initPhone = function()
-	setState('isDisabled', false)
-	TriggerEvent('dg-phone:load')
-	TriggerServerEvent('dg-phone:load')
-	SendAppEvent('phone', {
-		action = 'init',
-		data = GetCurrentResourceName()
-	})
+  setState('isDisabled', false)
+  TriggerEvent('dg-phone:load')
+  TriggerServerEvent('dg-phone:load')
+  SendAppEvent('phone', {
+    action = 'init',
+    data = GetCurrentResourceName()
+  })
 end
 
 unloadPhone = function()
@@ -38,47 +40,47 @@ end
 
 RegisterUICallback('phone/close', function(data, cb)
   if data.inCamera then return end
-	closePhone()
-	cb({data= {}, meta={ok=true, message="done"}})
+  closePhone(0, true)
+  cb({ data = {}, meta = { ok = true, message = "done" } })
 end)
 
 RegisterUICallback('phone/silence', function(data, cb)
-	setState('isMuted', data.silenced)
-	cb({data= {}, meta={ok=true, message="done"}})
+  setState('isMuted', data.silenced)
+  cb({ data = {}, meta = { ok = true, message = "done" } })
 end)
 
 RegisterNetEvent('dg-phone:client:togglePhone', function(toggle)
-	if toggle then
-		openPhone()
-	else
-		closePhone()
-	end
+  if toggle then
+    openPhone()
+  else
+    closePhone()
+  end
 end)
 
 RegisterNetEvent('DGCore:client:playerLoaded', function()
-	setState('characterLoaded', true)
+  setState('characterLoaded', true)
 end)
 
 RegisterNetEvent('DGCore:client:playerUnloaded', function()
-	closePhone()
-	setState('isDisabled', true)
-	setState('characterLoaded', false)
+  closePhone()
+  setState('isDisabled', true)
+  setState('characterLoaded', false)
 end)
 
 RegisterNetEvent('onResourceStart', function(res)
-	if res == GetCurrentResourceName() then
-		while (not DGCore) do Wait(10) end
-		if DGCore.Functions.GetPlayerData() ~= nil then
+  if res == GetCurrentResourceName() then
+    while (not DGCore) do Wait(10) end
+    if DGCore.Functions.GetPlayerData() ~= nil then
       initPhone()
-			setState('characterLoaded', true)
-		end
-	end
+      setState('characterLoaded', true)
+    end
+  end
 end)
 
 RegisterNetEvent('onResourceStop', function(res)
-	if res == GetCurrentResourceName() then
+  if res == GetCurrentResourceName() then
     unloadPhone()
-	end
+  end
 end)
 
 RegisterNetEvent('dg-ui:loadData', function()
@@ -86,33 +88,35 @@ RegisterNetEvent('dg-ui:loadData', function()
 end)
 
 RegisterNetEvent('dg-ui:reload', function()
-  if not getState('characterLoaded') then return	end
+  if not getState('characterLoaded') then return end
   unloadPhone()
 end)
 
 --region Keybindings
 RegisterNetEvent('dg-lib:keyEvent')
 AddEventHandler('dg-lib:keyEvent', function(name, isDown)
-	if not isDown or not canOpen() then return end
-	if name == "openPhone" then
-		openPhone()
-	end
-	if name == 'acceptNotification' then
-		SendNUIMessage({
+  if not isDown or not canOpen() then return end
+  if name == "openPhone" then
+    openPhone()
+  end
+  if name == 'acceptNotification' then
+    SendNUIMessage({
       appName = 'home-screen',
-			action = 'acceptNotification'
-		})
-	end
-	if name == "declineNotification" then
-		SendNUIMessage({
+      action = 'acceptNotification'
+    })
+  end
+  if name == "declineNotification" then
+    SendNUIMessage({
       appName = 'home-screen',
-			action = 'declineNotification'
-		})
-	end
+      action = 'declineNotification'
+    })
+  end
 end)
 
 exports["dg-lib"]:registerKeyMapping("openPhone", "(phone) Open jouw telefoon", '+openPhone', '-openPhone', "M", true)
-exports["dg-lib"]:registerKeyMapping("acceptNotification", "(phone) Accepteer melding", '+acceptNotification', '-acceptNotification', "", true)
-exports["dg-lib"]:registerKeyMapping("declineNotification", "(phone) Weiger melding", '+declineNotification', '-declineNotification', "", true)
+exports["dg-lib"]:registerKeyMapping("acceptNotification", "(phone) Accepteer melding", '+acceptNotification',
+  '-acceptNotification', "", true)
+exports["dg-lib"]:registerKeyMapping("declineNotification", "(phone) Weiger melding", '+declineNotification',
+  '-declineNotification', "", true)
 
 --endregion
