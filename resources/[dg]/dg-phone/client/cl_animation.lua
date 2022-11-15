edb9fa276d2aad84e7627d901c05ab5e8467e267
NuttyShrimp <jan.lecoutere@hotmail.be>
2022-11-15 01:00:57 +0100

    fix(phone): fixed call animation scuff

--- resources/[dg]/dg-phone/client/cl_animation.lua
+++ resources/[dg]/dg-phone/client/cl_animation.lua
@@ -2,158 +2,166 @@ local isLoopRunning = false
 local phoneProp = nil
 
 setPhoneState = function(state)
-	isLoopRunning = state ~= 0
-	if (isLoopRunning) then
-		while getState('state') ~= 0 do
-			local ped = PlayerPedId()
-			if getState('state') == 2 then
-				openCamera()
-			elseif getState('inCall') then
-				handleCallAnimation(ped)
-			elseif getState('state') == 1 then
-				handleOpenAnimation(ped)
-			end
-			if not phoneProp then
-				phoneProp = exports["dg-propattach"]:add("phone")
-			end
-			Wait(250)
-		end
-	end
-	if state == 0 and state ~= oldContext.state then
-		if not getState('inCall') then
-			handleCloseAnimation(PlayerPedId())
-		end
-	end
+  isLoopRunning = state ~= 0
+  if (isLoopRunning) then
+    while getState('state') ~= 0 or getState('inCall') do
+      local ped = PlayerPedId()
+      if getState('state') == 2 then
+        openCamera()
+      elseif getState('inCall') then
+        handleCallAnimation(ped)
+      elseif getState('state') == 1 then
+        handleOpenAnimation(ped)
+      end
+      if not phoneProp then
+        phoneProp = exports["dg-propattach"]:add("phone")
+      end
+      Wait(250)
+    end
+  end
+  if state == 0 and state ~= oldContext.state then
+    if not getState('inCall') then
+      handleCloseAnimation(PlayerPedId())
+    end
+  end
 end
 
 setPhoneCallAnim = function(toggle)
-	if toggle then
-		handleCallAnimation(PlayerPedId())
-	else
-		handleCallEndAnimation(PlayerPedId())
-	end
+  if toggle then
+    handleCallAnimation(PlayerPedId())
+  else
+    handleCallEndAnimation(PlayerPedId())
+  end
 end
 
 StopAllAnimation = function()
-	local ped = PlayerPedId()
-	isLoopRunning = false
-	closeCamera()
-	handleCallEndAnimation(ped)
-	handleCloseAnimation(ped)
-	if phoneProp then
-		exports['dg-propattach']:remove(phoneProp);
+  local ped = PlayerPedId()
+  isLoopRunning = false
+  closeCamera()
+  handleCallEndAnimation(ped)
+  handleCloseAnimation(ped)
+  if phoneProp then
+    exports['dg-propattach']:remove(phoneProp);
     phoneProp = nil
-	end
+  end
 end
 
 -- Local animation functions
 
 LoadAnimDict = function(dict)
-	while not HasAnimDictLoaded(dict) do
-		RequestAnimDict(dict)
-		Citizen.Wait(5)
-	end
+  while not HasAnimDictLoaded(dict) do
+    RequestAnimDict(dict)
+    Citizen.Wait(5)
+  end
 end
 
 doAnimation = function(ped, dict, anim, speed, speedOut, flag)
-	if speedOut == nil then speedOut = -1 end
-	if flag == nil then flag = 50 end
-	LoadAnimDict(dict)
-	if (not IsEntityPlayingAnim(ped, dict, anim, 3)) then
-		SetCurrentPedWeapon(ped, `weapon_unarmed`, true);
-		TaskPlayAnim(ped, dict, anim, speed, speedOut, -1, flag, 0, false, false, false);
-	end
+  if speedOut == nil then speedOut = -1 end
+  if flag == nil then flag = 50 end
+  LoadAnimDict(dict)
+  if (not IsEntityPlayingAnim(ped, dict, anim, 3)) then
+    SetCurrentPedWeapon(ped, `weapon_unarmed`, true);
+    TaskPlayAnim(ped, dict, anim, speed, speedOut, -1, flag, 0, false, false, false);
+  end
 end
 
 handleCallAnimation = function(ped)
-	if (IsPedInAnyVehicle(ped, false)) then
-		handleOnCallVehicleAnim(ped)
-	else
-		handleOnCallNormalAnim(ped)
-	end
+  if (IsPedInAnyVehicle(ped, false)) then
+    handleOnCallVehicleAnim(ped)
+  else
+    handleOnCallNormalAnim(ped)
+  end
 end
 
 handleCallEndAnimation = function(ped)
-	if (IsPedInAnyVehicle(ped, false)) then
-		handleCallEndVehicleAnim(ped)
-	else
-		handleCallEndNormalAnim(ped)
-	end
+  if (IsPedInAnyVehicle(ped, false)) then
+    handleCallEndVehicleAnim(ped)
+  else
+    handleCallEndNormalAnim(ped)
+  end
 end
 
 handleOpenAnimation = function(ped)
-	if (IsPedInAnyVehicle(ped, false)) then
-		handleOpenVehicleAnim(ped)
-	else
-		handleOpenNormalAnim(ped)
-	end
+  if (IsPedInAnyVehicle(ped, false)) then
+    handleOpenVehicleAnim(ped)
+  else
+    handleOpenNormalAnim(ped)
+  end
 end
 
 handleCloseAnimation = function(ped)
-	if (IsPedInAnyVehicle(ped, false)) then
-		handleCloseVehicleAnim(ped)
-	else
-		handleCloseNormalAnim(ped)
-	end
+  if (IsPedInAnyVehicle(ped, false)) then
+    handleCloseVehicleAnim(ped)
+  else
+    handleCloseNormalAnim(ped)
+  end
 end
 
 --region functions
 
 handleOpenVehicleAnim = function(ped)
-	local dict = 'cellphone@in_car@ps'
-	local anim = 'cellphone_text_in';
-	doAnimation(ped, dict, anim, 7.0)
+  local dict = 'cellphone@in_car@ps'
+  local anim = 'cellphone_text_in';
+  doAnimation(ped, dict, anim, 7.0)
 end
 
 handleOpenNormalAnim = function(ped)
-	local dict = 'cellphone@';
-	local anim = 'cellphone_text_in';
-	doAnimation(ped, dict, anim, 8.0)
+  local dict = 'cellphone@';
+  local anim = 'cellphone_text_in';
+  doAnimation(ped, dict, anim, 8.0)
 end
 
 handleCloseVehicleAnim = function(ped)
-	local dict = 'cellphone@in_car@ps';
-	StopAnimTask(ped, dict, 'cellphone_text_in', 1.0);
-	StopAnimTask(ped, dict, 'cellphone_call_to_text', 1.0);
-	exports['dg-propattach']:remove(phoneProp);
-	phoneProp = nil
+  local dict = 'cellphone@in_car@ps';
+  StopAnimTask(ped, dict, 'cellphone_text_in', 1.0);
+  StopAnimTask(ped, dict, 'cellphone_call_to_text', 1.0);
+  exports['dg-propattach']:remove(phoneProp);
+  phoneProp = nil
 end
 
 handleCloseNormalAnim = function(ped)
-	local dict = 'cellphone@';
-	local anim = 'cellphone_text_out';
-	StopAnimTask(playerPed, dict, 'cellphone_text_in', 1.0);
-	Citizen.Wait(100)
-	doAnimation(ped, dict, anim, 7.0)
-	Citizen.Wait(200)
-	StopAnimTask(ped, dict, anim, 1.0);
-	exports['dg-propattach']:remove(phoneProp);
-	phoneProp = nil
+  local dict = 'cellphone@';
+  local anim = 'cellphone_text_out';
+  StopAnimTask(playerPed, dict, 'cellphone_text_in', 1.0);
+  Citizen.Wait(100)
+  doAnimation(ped, dict, anim, 7.0)
+  Citizen.Wait(200)
+  StopAnimTask(ped, dict, anim, 1.0);
+  exports['dg-propattach']:remove(phoneProp);
+  phoneProp = nil
 end
 
 handleOnCallVehicleAnim = function(ped)
-	local dict = 'cellphone@in_car@ps';
-	local anim = 'cellphone_call_listen_base';
-	doAnimation(ped, dict, anim, 3.0, 3.0, 49)
+  local dict = 'cellphone@in_car@ps';
+  local anim = 'cellphone_call_listen_base';
+  doAnimation(ped, dict, anim, 3.0, 3.0, 49)
 end
 
 handleOnCallNormalAnim = function(ped)
-	local dict = 'cellphone@';
-	local anim = 'cellphone_call_listen_base';
-	doAnimation(ped, dict, anim, 3.0, 3.0, 49)
+  local dict = 'cellphone@';
+  local anim = 'cellphone_call_listen_base';
+  doAnimation(ped, dict, anim, 3.0, 3.0, 49)
 end
 
 handleCallEndVehicleAnim = function(ped)
-	local dict = 'cellphone@in_car@ps';
-	local anim = 'cellphone_call_to_text';
-	StopAnimTask(ped, dict, 'cellphone_call_listen_base', 1.0);
-	doAnimation(ped, dict, anim, 1.3, 5.0)
+  local dict = 'cellphone@in_car@ps';
+  local anim = 'cellphone_call_to_text';
+  StopAnimTask(ped, dict, 'cellphone_call_listen_base', 1.0);
+  doAnimation(ped, dict, anim, 1.3, 5.0)
+  Wait(500)
+  if getState('state') ~= 1 then
+    handleCloseVehicleAnim(ped)
+  end
 end
 
 handleCallEndNormalAnim = function(ped)
-	local dict = 'cellphone@';
-	local anim = 'cellphone_call_to_text';
-	doAnimation(ped, dict, anim, 2.5, 8.0)
+  local dict = 'cellphone@';
+  local anim = 'cellphone_call_to_text';
+  doAnimation(ped, dict, anim, 2.5, 8.0)
+  Wait(500)
+  if getState('state') ~= 1 then
+    handleCloseNormalAnim(ped)
+  end
 end
 
 --endregion
@@ -162,42 +170,44 @@ end
 local isFrontCam = false
 
 CellFrontCamActivate = function(activate)
-	return Citizen.InvokeNative(0x2491A93618B7D838, activate)
+  return Citizen.InvokeNative(0x2491A93618B7D838, activate)
 end
 
-exports["dg-lib"]:registerKeyMapping('switchPhoneCamera', 'Verander phone camera', '+switchPhoneCamera', '-switchPhoneCamera', 'UP', true)
+exports["dg-lib"]:registerKeyMapping('switchPhoneCamera', 'Verander phone camera', '+switchPhoneCamera',
+  '-switchPhoneCamera', 'UP', true)
 
-exports["dg-lib"]:registerKeyMapping('takePhoneCameraPicture', 'Neem photo (phone)', '+takePhoneCameraPicture', '-takePhoneCameraPicture', 'RETURN', true)
+exports["dg-lib"]:registerKeyMapping('takePhoneCameraPicture', 'Neem photo (phone)', '+takePhoneCameraPicture',
+  '-takePhoneCameraPicture', 'RETURN', true)
 
 AddEventHandler('dg-lib:keyEvent', function(name, isDown)
-	if getState('state') ~= 2 or not isDown  then return	end
-	if name == "takePhoneCameraPicture" then
-		setState('state', 1)
-		local event = DGCore.Functions.TriggerCallback('dg-phone:server:photo:take', nil)
-	elseif name == "switchPhoneCamera" then
-		isFrontCam = not isFrontCam
-		CellFrontCamActivate(isFrontCam)
-	end
+  if getState('state') ~= 2 or not isDown then return end
+  if name == "takePhoneCameraPicture" then
+    setState('state', 1)
+    local event = DGCore.Functions.TriggerCallback('dg-phone:server:photo:take', nil)
+  elseif name == "switchPhoneCamera" then
+    isFrontCam = not isFrontCam
+    CellFrontCamActivate(isFrontCam)
+  end
 end)
 
 openCamera = function()
-	isFrontCam = false
-	CreateMobilePhone(phoneId)
-	CellCamActivate(true, true)
-	while getState('state') == 2 do
-		if IsControlJustPressed(0, 177) then
-			break
-		end
-		Wait(0)
-	end
-	closeCamera()
-	openPhone()
+  isFrontCam = false
+  CreateMobilePhone(phoneId)
+  CellCamActivate(true, true)
+  while getState('state') == 2 do
+    if IsControlJustPressed(0, 177) then
+      break
+    end
+    Wait(0)
+  end
+  closeCamera()
+  openPhone()
 end
 
 closeCamera = function()
-	DestroyMobilePhone()
-	CellCamActivate(false, false)
-	CellFrontCamActivate(false)
+  DestroyMobilePhone()
+  CellCamActivate(false, false)
+  CellFrontCamActivate(false)
 end
 
 --endregion
--- resources/[dg]/dg-phone/client/cl_calls.lua
+++ resources/[dg]/dg-phone/client/cl_calls.lua
@@ -1,128 +1,132 @@
 local soundInfo = {
-	dial = {
-		name = "phone_dial",
-		ids = {}
-	},
-	ring = {
-		name = "phone_ringtone",
-		ids = {}
-	},
+  dial = {
+    name = "phone_dial",
+    ids = {}
+  },
+  ring = {
+    name = "phone_ringtone",
+    ids = {}
+  },
 }
 
 playSound = function(type, id)
-	if getState('isMuted') then return end
-	exports["nutty-sounds"]:playSoundOnEntity(('phone_call_%s_%s'):format(type, id), soundInfo[type].name, 'DLC_NUTTY_SOUNDS', PlayerPedId())
-	soundInfo[type].ids[id] = true
+  if getState('isMuted') then return end
+  exports["nutty-sounds"]:playSoundOnEntity(('phone_call_%s_%s'):format(type, id), soundInfo[type].name,
+    'DLC_NUTTY_SOUNDS', PlayerPedId())
+  soundInfo[type].ids[id] = true
 end
 
 stopSounds = function(id)
-	for k, v in pairs(soundInfo) do
-		if v.ids[id] then
-			exports["nutty-sounds"]:stopSound(('phone_call_%s_%s'):format(k, id))
-			v.ids[id] = nil
-		end
-	end
+  for k, v in pairs(soundInfo) do
+    if v.ids[id] then
+      exports["nutty-sounds"]:stopSound(('phone_call_%s_%s'):format(k, id))
+      v.ids[id] = nil
+    end
+  end
 end
 
 RegisterUICallback('phone/startCall', function(data, cb)
-	local soundId = DGCore.Functions.TriggerCallback('dg-phone:server:startCall', nil, data)
-	if soundId then
-		playSound('dial', soundId)
-	end
-	cb({ data = {}, meta = { ok = true, message = "done" } })
+  local soundId = DGCore.Functions.TriggerCallback('dg-phone:server:startCall', nil, data)
+  if soundId then
+    playSound('dial', soundId)
+  end
+  setState("inCall", true)
+  cb({ data = {}, meta = { ok = true, message = "done" } })
 end)
 
 RegisterUICallback('phone/dispatchEndCall', function(data, cb)
-	DGCore.Functions.TriggerCallback('dg-phone:server:endCall', nil, data)
-	cb({ data = {}, meta = { ok = true, message = "done" } })
+  DGCore.Functions.TriggerCallback('dg-phone:server:endCall', nil, data)
+  setState("inCall", false)
+  cb({ data = {}, meta = { ok = true, message = "done" } })
 end)
 
 RegisterUICallback('phone/acceptCall', function(data, cb)
-	DGCore.Functions.TriggerCallback('dg-phone:server:initiateCall', nil, data)
-	cb({ data = {}, meta = { ok = true, message = "done" } })
+  DGCore.Functions.TriggerCallback('dg-phone:server:initiateCall', nil, data)
+  cb({ data = {}, meta = { ok = true, message = "done" } })
 end)
 
 RegisterUICallback('phone/declineCall', function(data, cb)
-	DGCore.Functions.TriggerCallback('dg-phone:server:endCall', nil, data)
-	cb({ data = {}, meta = { ok = true, message = "done" } })
+  DGCore.Functions.TriggerCallback('dg-phone:server:endCall', nil, data)
+  cb({ data = {}, meta = { ok = true, message = "done" } })
 end)
 
 RegisterUICallback('phone/endcall', function(data, cb)
-	cb({ data = {}, meta = { ok = true, message = "done" } })
+  cb({ data = {}, meta = { ok = true, message = "done" } })
 end)
 
 RegisterNetEvent('dg-phone:client:incomingCall')
 AddEventHandler('dg-phone:client:incomingCall', function(data)
-	playSound('ring', data.soundId)
-	SendAppEvent('phone',{
-		appName = "phone",
-		action = 'incomingCall',
-		data = data
-	})
+  playSound('ring', data.soundId)
+  SendAppEvent('phone', {
+    appName = "phone",
+    action = 'incomingCall',
+    data = data
+  })
 end)
 
 RegisterNetEvent('dg-phone:client:endCurrentCall', function(soundId)
-	stopSounds(soundId)
-	SendAppEvent('phone',{
-		appName = "phone",
-		action = 'endCurrentCall',
-		data = {}
-	})
+  stopSounds(soundId)
+  setState("inCall", false)
+  SendAppEvent('phone', {
+    appName = "phone",
+    action = 'endCurrentCall',
+    data = {}
+  })
 end)
 
 RegisterNetEvent('dg-phone:client:initiateCall', function(soundId)
-	stopSounds(soundId)
-	SendAppEvent('phone',{
-		appName = "phone",
-		action = "setCallActive",
-		data = {}
-	})
+  stopSounds(soundId)
+  SendAppEvent('phone', {
+    appName = "phone",
+    action = "setCallActive",
+    data = {}
+  })
 end)
 
 -- Anon calls
 Citizen.CreateThread(function()
-	exports['dg-peek']:addModelEntry({
-		'p_phonebox_02_s',
-		'prop_phonebox_03',
-		'prop_phonebox_02',
-		'prop_phonebox_04',
-		'prop_phonebox_01c',
-		'prop_phonebox_01a',
-		'prop_phonebox_01b',
-		'p_phonebox_01b_s',
-	}, {
-		options = {
-			{
-				icon = 'fas fa-phone',
-				label = 'Use Payphone',
-				item = 'phone',
-				action = function(entity)
-					local dialog = exports['dg-ui']:openInput({
-						header = "Make a call",
-						inputs = {
-							{
-								label = "Phone Number",
-								name = "phoneNumber",
-								type = "number",
-							},
-						},
-					})
+  exports['dg-peek']:addModelEntry({
+    'p_phonebox_02_s',
+    'prop_phonebox_03',
+    'prop_phonebox_02',
+    'prop_phonebox_04',
+    'prop_phonebox_01c',
+    'prop_phonebox_01a',
+    'prop_phonebox_01b',
+    'p_phonebox_01b_s',
+  }, {
+    options = {
+      {
+        icon = 'fas fa-phone',
+        label = 'Use Payphone',
+        item = 'phone',
+        action = function(entity)
+          local dialog = exports['dg-ui']:openInput({
+            header = "Make a call",
+            inputs = {
+              {
+                label = "Phone Number",
+                name = "phoneNumber",
+                type = "number",
+              },
+            },
+          })
 
-					if dialog ~= nil then
-						if (dialog.phoneNumber) then
-							SendAppEvent('phone',{
+          if dialog ~= nil then
+            if (dialog.phoneNumber) then
+              SendAppEvent('phone', {
                 appName = "phone",
-								action = 'startAnonCall',
-								data = dialog.phoneNumber
-							})
-						end
-					end
-				end,
-				canInteract = function(entity, distance, data)
-					return not exports["qb-prison"]:isInJail()
-				end,
-			}
-		},
-		distance = 1.5,
-	})
+                action = 'startAnonCall',
+                data = dialog.phoneNumber
+              })
+            end
+          end
+        end,
+        canInteract = function(entity, distance, data)
+          return not exports["qb-prison"]:isInJail()
+        end,
+      }
+    },
+    distance = 1.5,
+  })
 end)
