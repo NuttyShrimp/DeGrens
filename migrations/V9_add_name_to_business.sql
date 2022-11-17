9bb5f3f03c04dcf7844719f81ac0097cb821509b
NuttyShrimp <jan.lecoutere@hotmail.be>
2022-11-17 23:02:00 +0100

    fix(migrations): fixed seeded businesses info

--- migrations/V9_add_name_to_business.sql
+++ migrations/V9_add_name_to_business.sql
@@ -3,7 +3,11 @@ ALTER TABLE business
 
 UPDATE business
 SET name = 'dff'
-WHERE id = 1;
+WHERE id = 1 AND label='De FliereFluiters';
+
+UPDATE business
+SET name = 'pdm'
+WHERE id = 2 AND label="PDM";
 
 ALTER TABLE business
   MODIFY COLUMN name VARCHAR(255) NOT NULL UNIQUE;
