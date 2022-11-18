3376ec98d0ff79023fc797bbe689910fb5446220
NuttyShrimp <jan.lecoutere@hotmail.be>
2022-11-18 01:11:41 +0100

    fix(scenes): fixed manifest

--- resources/[dg]/dg-scenes/fxmanifest.lua
+++ resources/[dg]/dg-scenes/fxmanifest.lua
@@ -1,21 +1,22 @@
 fx_version 'cerulean'
 game 'gta5'
 
+shared_script '@dg-core/import.js'
 client_scripts {
-    '@ts-shared/client/client.js',
-    '@dg-lib/client/cl_ui.lua',
-    'client/cl_*.lua'
+  '@ts-shared/client/client.js',
+  '@dg-lib/client/cl_ui.lua',
+  'client/cl_*.lua'
 }
 
 server_scripts {
-    '@ts-shared/server/server.js',
-    'server/sv_*.lua',
+  '@ts-shared/server/server.js',
+  'server/sv_*.lua',
 }
 
 shared_scripts {
-    '@dg-core/import.lua',
-    '@ts-shared/shared/lib.lua',
-	'config.lua'
+  '@dg-core/import.lua',
+  '@ts-shared/shared/lib.lua',
+  'config.lua'
 }
 
 client_script "@dg-logs/client/cl_log.lua"
