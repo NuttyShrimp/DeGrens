c30313d37d03502c4dc6bfad46a3f2c251bf4ad5
NuttyShrimp <jan.lecoutere@hotmail.be>
2022-11-29 00:17:37 +0100

    fix(core): wait until exports are available

--- resources/[core]/dg-core/import.js
+++ resources/[core]/dg-core/import.js
@@ -1,9 +1,17 @@
 if (GetResourceState('dg-core') == 'started') {
+  (async () => {
+    while (exports?.['dg-core']?.GetSharedObject === undefined) {
+      await new Promise(r => setTimeout(r, 100));
+    }
+  })();
   global.DGCore = exports['dg-core'].GetSharedObject();
 }
 
-on('onResourceStart', res => {
+on('onResourceStart', async res => {
   if (res == 'dg-core') {
+    while (exports?.['dg-core']?.GetSharedObject === undefined) {
+      await new Promise(r => setTimeout(r, 100));
+    }
     global.DGCore = exports['dg-core'].GetSharedObject();
   }
 });
