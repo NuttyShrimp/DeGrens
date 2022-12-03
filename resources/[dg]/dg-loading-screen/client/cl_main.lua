90aceec4cc53171e4c2f1de87aa5e2942475a69b
Jens Galle <galle.jens@telenet.be>
2022-12-03 01:21:50 +0100

    fix(loadingscreen): hopefully fix double loadingscreen

--- resources/[dg]/dg-loading-screen/client/cl_main.lua
+++ resources/[dg]/dg-loading-screen/client/cl_main.lua
@@ -1,14 +1,25 @@
 local called = false
 
+local stopLoadingScreen = function()
+  SendLoadingScreenMessage(json.encode({
+    shutDown = true
+  }))
+  ShutdownLoadingScreen()
+  ShutdownLoadingScreenNui()
+end
+  
 RegisterNetEvent('loadscreen:disableScreen')
 AddEventHandler('loadscreen:disableScreen', function()
-  if not called then
-    SendLoadingScreenMessage(json.encode({
-      shutDown = true
-    }))
-    called = true
-    ShutdownLoadingScreenNui()
-    ShutdownLoadingScreen()
+  if called then return end
+
+  called = true
+
+  -- Debugging shit and seeing if this works to prevent double loadingscreen
+  while true do
+    stopLoadingScreen()
+    Wait(1000)
+    if not GetIsLoadingScreenActive() then break end
+    print('Failed to shutdown loading screen. Trying again...')
   end
 end)
 
