a45140cec3423886e75966e2f1c6d266cd189334
Jens Galle <galle.jens@telenet.be>
2022-12-06 21:08:00 +0100

    fix(admin): fix cloak state being swapped

--- resources/[dg]/dg-admin/typescript/src/server/modules/commands/data/cloak.ts
+++ resources/[dg]/dg-admin/typescript/src/server/modules/commands/data/cloak.ts
@@ -1,4 +1,4 @@
-import { Events, Notifications, Sync } from '@dgx/server';
+import { Notifications, Sync } from '@dgx/server';
 
 let cloakToggled: Record<number, boolean> = {};
 
@@ -12,7 +12,7 @@ export const cloak: CommandData = {
     // argument is undefined when using bind, so save state and toggle every func call
     const toggle = !cloakToggled[caller.source];
     cloakToggled[caller.source] = toggle;
-    Sync.setPlayerVisible(caller.source, toggle);
+    Sync.setPlayerVisible(caller.source, !toggle);
     Notifications.add(caller.source, `Cloak ${toggle ? 'enabled' : 'disabled'}`);
   },
   UI: {
