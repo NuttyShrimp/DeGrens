935dcd5d4f76ec159814df8baaa0502ce59ad853
NuttyShrimp <jan.lecoutere@hotmail.be>
2022-12-31 17:17:49 +0100

    feat(auth): extend key expiry date & add check for tokens from previous restart

--- resources/[dg]/dg-auth/typescript/src/server/modules/authTokens/service.ts
+++ resources/[dg]/dg-auth/typescript/src/server/modules/authTokens/service.ts
@@ -10,16 +10,21 @@ import { Admin } from '@dgx/server';
 
 let signer: any;
 let verifier: any;
+// The earliest moment a player can receive a valid token
+let startSignTimestamp = Date.now();
 
 export const createJWSHandlers = () => {
   signer = createSigner({
-    expiresIn: 1000 * 60 * 60 * 12,
+    // Normally on 12 hours needed but is as safety measurement when server skips a restart or so
+    expiresIn: 1000 * 60 * 60 * 25,
     key: getPrivateToken(),
   });
 
   verifier = createVerifier({
     key: getPrivateToken(),
   });
+
+  startSignTimestamp = Date.now();
 }
 
 export const createResourceToken = (src: number, resource: string) => {
@@ -53,6 +58,14 @@ export const validateToken = (src: number, resource: string, token: string) => {
   }
   try {
     const data = verifier(token) as ResourceTokenData;
+    if (data.timeStamp < startSignTimestamp) {
+      Admin.ACBan(src, 'Unauthorized event triggering (old token)');
+      mainLogger.info(
+        `${GetPlayerName(String(src))} tried to use an old token | token.steamId: ${data.steamId
+        } | token.resource: ${data.resource}`
+      );
+      return false;
+    }
     if (!data?.steamId || !data?.resource) {
       Admin.ACBan(src, 'Unauthorized event triggering (invalid session token)');
       mainLogger.info(
