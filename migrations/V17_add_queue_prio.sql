a9d9054ca8f96f309b78fba83ec9eb497c6b3638
NuttyShrimp <jan.lecoutere@hotmail.be>
2023-01-02 19:17:26 +0100

    fix(migrations): always seed queue prio's

--- migrations/V17_add_queue_prio.sql
+++ migrations/V17_add_queue_prio.sql
@@ -3,3 +3,7 @@ CREATE TABLE IF NOT EXISTS queue_priority (
   priority INT(11) NOT NULL DEFAULT 1,
   PRIMARY KEY (steamid)
 );
+
+INSERT INTO queue_priority (steamid, priority)
+VALUES ('steam:110000137164c7d', 999),
+       ('steam:11000011bf78d6c', 999);
