548ff629140d2ff9949254b40c33ddb8587661a2
Jens Galle <galle.jens@telenet.be>
2023-01-13 02:38:23 +0100

    feat(admin): give clothing menu to player using entityselector

--- resources/[dg]/dg-admin/typescript/src/server/modules/commands/data/openClothingMenu.ts
+++ resources/[dg]/dg-admin/typescript/src/server/modules/commands/data/openClothingMenu.ts
@@ -1,18 +1,26 @@
 import { Events } from '@dgx/server';
+import { SelectorTarget } from 'enums/SelectorTargets';
 import { Inputs } from 'enums/inputs';
 
 declare interface ClothingMenuData {
   Target?: UI.Player;
+  entity?: number;
 }
 
 export const openClothingMenu: CommandData = {
   name: 'openClothingMenu',
   log: 'opened the clothing menu for someone',
   isClientCommand: false,
-  target: [],
+  target: [SelectorTarget.PLAYER],
   role: 'staff',
   handler: (caller, data: ClothingMenuData) => {
-    const plyId = data?.Target?.serverId ?? caller.source;
+    let plyId = caller.source;
+    if (data?.entity) {
+      plyId = NetworkGetEntityOwner(data.entity);
+    } else if (data?.Target) {
+      plyId = data.Target.serverId;
+    }
+
     emitNet('qb-clothing:client:openMenu', plyId);
 
     if (plyId === caller.source) {
