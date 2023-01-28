5987a5729ec21c9fc3bdf16c80d6cca1a11aef77
Jens Galle <galle.jens@telenet.be>
2023-01-29 00:40:49 +0100

    fix(inventory): remove old code

--- resources/[dg]/dg-inventory/typescript/src/client/client.ts
+++ resources/[dg]/dg-inventory/typescript/src/client/client.ts
@@ -2,9 +2,3 @@ import './controllers';
 import './modules';
 import './classes/contextmanager';
 import './services/cache';
-
-import objectsManager from 'modules/objects/classes/objectsmanager';
-
-setImmediate(() => {
-  objectsManager.fetchConfig();
-});
