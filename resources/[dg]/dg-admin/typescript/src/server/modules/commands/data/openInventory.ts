0020879d65469f190278b2d8b7c80acbcdae25aa
Jens Galle <galle.jens@telenet.be>
2023-01-31 17:09:47 +0100

    fix(admin): fix openInv by entity selector returning early

--- resources/[dg]/dg-admin/typescript/src/server/modules/commands/data/openInventory.ts
+++ resources/[dg]/dg-admin/typescript/src/server/modules/commands/data/openInventory.ts
@@ -25,10 +25,7 @@ export const openInventory: CommandData = {
       }
       inv = args.inventoryId;
     } else {
-      if (!args.Target?.serverId) {
-        return;
-      }
-      const targetPlayer = args.entity ? NetworkGetEntityOwner(args.entity) : args.Target.serverId;
+      const targetPlayer = args.entity ? NetworkGetEntityOwner(args.entity) : args.Target?.serverId;
       if (!targetPlayer) {
         Notifications.add(caller.source, 'Je moet een target invullen', 'error');
         return;
