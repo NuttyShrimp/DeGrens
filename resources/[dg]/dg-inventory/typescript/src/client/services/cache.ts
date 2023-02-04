f1fbb015d75a6dc9dcb060368e7dffe89646f6c6
Jens Galle <galle.jens@telenet.be>
2023-02-04 13:46:05 +0100

    fix(inventory): fix itemname caching

--- resources/[dg]/dg-inventory/typescript/src/client/services/cache.ts
+++ resources/[dg]/dg-inventory/typescript/src/client/services/cache.ts
@@ -1,16 +1,20 @@
 import { Events } from '@dgx/client';
 
-const items: Set<string> = new Set();
+const items: string[] = [];
 
 Events.onNet('inventory:client:updateCache', (action: 'add' | 'remove', itemName: string) => {
   switch (action) {
     case 'add':
-      items.add(itemName);
+      items.push(itemName);
       break;
     case 'remove':
-      items.delete(itemName);
+      const idx = items.indexOf(itemName);
+      if (idx !== -1) {
+        items[idx] = items[items.length - 1];
+        items.pop();
+      }
       break;
   }
 });
 
-global.exports('getAllItemNames', () => [...items]);
+global.exports('getAllItemNames', () => items);
