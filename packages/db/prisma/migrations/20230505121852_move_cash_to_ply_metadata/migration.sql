ae9bc26f76bf86e645e0514a4816f9db4127221b
NuttyShrimp <jan.lecoutere@hotmail.be>
2023-05-05 19:36:45 +0200

    fix(migrations): update view in cash update migration

--- packages/db/prisma/migrations/20230505121852_move_cash_to_ply_metadata/migration.sql
+++ packages/db/prisma/migrations/20230505121852_move_cash_to_ply_metadata/migration.sql
@@ -1,2 +1,15 @@
 UPDATE character_data as cd SET metadata = JSON_INSERT(metadata, '$.cash', (SELECT cash FROM character_info as ci WHERE ci.citizenid = cd.citizenid));
 ALTER TABLE character_info DROP cash;
+
+CREATE OR REPLACE VIEW all_character_data AS SELECT c.*,
+       ci.firstname,
+       ci.lastname,
+       ci.birthdate,
+       ci.gender,
+       ci.nationality,
+       ci.phone,
+       cd.position,
+       cd.metadata
+FROM characters AS c
+         LEFT JOIN character_info AS ci ON ci.citizenid = c.citizenid
+         LEFT JOIN character_data AS cd ON cd.citizenid = c.citizenid;
