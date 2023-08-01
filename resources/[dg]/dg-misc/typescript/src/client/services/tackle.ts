70aaa3f7f0c4a0f135560784cb2c9ae3c5d1f073
Jens Galle <galle.jens@telenet.be>
2023-08-01 02:30:13 +0200

    feat(tackle): restrict usage some more

--- resources/[dg]/dg-misc/typescript/src/client/services/tackle.ts
+++ resources/[dg]/dg-misc/typescript/src/client/services/tackle.ts
@@ -1,4 +1,4 @@
-import { Events, Keys, Police, Util } from '@dgx/client';
+import { Events, Keys, Police, Util, Weapons } from '@dgx/client';
 
 Events.onNet('misc:tackle:do', () => {
   SetPedToRagdoll(
@@ -13,22 +13,16 @@ Events.onNet('misc:tackle:do', () => {
 });
 
 const tackleInit = async () => {
-  const ped = PlayerPedId();
-  if (
-    IsPedInAnyVehicle(ped, false) ||
-    GetEntitySpeed(ped) < 2.5 ||
-    IsPedRagdoll(ped) ||
-    Police.isCuffed() ||
-    Police.isEscorting() ||
-    !IsPedJumping(ped)
-  )
-    return;
+  if (!canTackle()) return;
 
   const closestPlayer = Util.getClosestPlayerInDistanceAndOutsideVehicle(2);
   if (!closestPlayer) return;
 
   Events.emitNet('misc:tackle:server', GetPlayerServerId(closestPlayer));
+
+  const ped = PlayerPedId();
   await Util.loadAnimDict('swimming@first_person@diving');
+
   if (IsEntityPlayingAnim(ped, 'swimming@first_person@diving', 'dive_run_fwd_-45_loop', 3)) {
     ClearPedTasksImmediately(ped);
   } else {
@@ -56,3 +50,20 @@ Keys.onPressDown('tackle', () => {
 });
 
 Keys.register('tackle', '(gov) iemand tacklen', 'LMENU');
+
+const canTackle = () => {
+  const ped = PlayerPedId();
+  return (
+    !IsPedInAnyVehicle(ped, true) &&
+    GetEntitySpeed(ped) > 2.5 &&
+    IsPedRunning(ped) &&
+    !IsPedRagdoll(ped) &&
+    !Police.isCuffed() &&
+    !Police.isEscorting() &&
+    !IsPedJumping(ped) &&
+    Weapons.getCurrentWeaponData() === null &&
+    !IsPedClimbing(ped) &&
+    !IsPedSwimming(ped) &&
+    !IsPedSwimmingUnderWater(ped)
+  );
+};
