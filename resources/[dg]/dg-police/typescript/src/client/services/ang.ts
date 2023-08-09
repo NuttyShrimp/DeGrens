1a4e1728aab7dfa50488cd55a88b40a911e4198a
NuttyShrimp <jan.lecoutere@hotmail.be>
2023-08-09 13:08:34 +0200

    feat(police/ang): unify az+ang + add opacity when iframe is not focused

--- resources/[dg]/dg-police/typescript/src/client/services/ang.ts
+++ resources/[dg]/dg-police/typescript/src/client/services/ang.ts
@@ -1,14 +1,23 @@
 import { Animations, Hospital, Jobs, Police } from '@dgx/client';
 
-on('police:openAng', () => {
-  if (Jobs.getCurrentJob()?.name !== 'police') return;
+const openAng = (prefix: string) => {
+  if (Jobs.getCurrentJob()?.name !== 'ambulance') return;
   if (Police.isCuffed() || Hospital.isDown()) return;
 
   Animations.startTabletAnimation();
   SetNuiFocus(true, true);
   SendNUIMessage({
     action: 'openAng',
+    site: prefix,
   });
+};
+
+on('hospital:openAng', () => {
+  openAng('az');
+});
+
+on('police:openAng', () => {
+  openAng('ang');
 });
 
 RegisterNuiCallbackType('angClosed');
