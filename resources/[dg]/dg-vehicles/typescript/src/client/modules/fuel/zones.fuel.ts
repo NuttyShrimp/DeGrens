2a9e3c6c418d5164eec33c83369addf57b1f436a
Jens Galle <galle.jens@telenet.be>
2023-08-17 00:59:12 +0200

    chore(vehicles/fuel): add missing gasstation

--- resources/[dg]/dg-vehicles/typescript/src/client/modules/fuel/zones.fuel.ts
+++ resources/[dg]/dg-vehicles/typescript/src/client/modules/fuel/zones.fuel.ts
@@ -439,6 +439,20 @@ const gasStationZones: {
     maxZ: 36.34,
     type: 'air',
   },
+
+  {
+    center: {
+      x: 175.78,
+      y: -1561.79,
+      z: 29.26,
+    },
+    width: 18.0,
+    length: 20.2,
+    heading: 45,
+    minZ: 28.26,
+    maxZ: 32.26,
+    type: 'land',
+  },
 ];
 
 let inZone = false;
