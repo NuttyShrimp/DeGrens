b2ea97f49bb852e88b576c11c54259321025332c
NuttyShrimp <jan.lecoutere@hotmail.be>
2023-08-17 13:55:29 +0200

    chore(db): fix migration

--- packages/db/prisma/migrations/20230809192714_add_mailbox_item_col/migration.sql
+++ packages/db/prisma/migrations/20230809192714_add_mailbox_item_col/migration.sql
@@ -1,6 +1,2 @@
 -- AlterTable
 ALTER TABLE `realestate_locations` ADD COLUMN `has_mailbox` VARCHAR(191) NOT NULL;
-
--- RedefineIndex
-CREATE UNIQUE INDEX `realestate_locations_name_key` ON `realestate_locations`(`name`);
-DROP INDEX `realestate_locations_name` ON `realestate_locations`;
