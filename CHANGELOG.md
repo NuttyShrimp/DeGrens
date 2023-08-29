a0b8c3b4df29bdba17683d5b9b03dc19a036912f
Jens Galle <galle.jens@telenet.be>
2023-08-29 14:08:32 +0200

    feat(phone/payconiq): display transaction success in modal

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -10,6 +10,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Added
 
 - Je kan nu zoeken op contactnaam in je telefoon tijdens overschrijvingen etc
+- Payconiq transaction modal geeft nu weer of transactie geslaagd is
 
 ### Changed
 
--- resources/[dg]/dg-financials/typescript/src/client/modules/payconiq/controller.payconiq.ts
+++ resources/[dg]/dg-financials/typescript/src/client/modules/payconiq/controller.payconiq.ts
@@ -12,8 +12,13 @@ UI.RegisterUICallback('phone/payconiq/get', async (_, cb) => {
 
 UI.RegisterUICallback('phone/payconiq/makeTransaction', async (data, cb) => {
   const account = await RPC.execute<IAccount>('financials:getDefaultAccount');
-  if (!account) throw new Error('Could not get default account');
+  if (!account) {
+    console.error('Could not get default account');
+    cb({ data: false, meta: { ok: true, message: 'done' } });
+    return;
+  }
+
   data.accountId = account.account_id;
-  await RPC.execute('financials:server:action:mobileTransaction', data);
-  cb({ data: {}, meta: { ok: true, message: 'done' } });
+  const success = await RPC.execute<boolean>('financials:server:action:mobileTransaction', data);
+  cb({ data: success, meta: { ok: true, message: 'done' } });
 });
