824092d53f35981432e2c8187ea165b7b3f0353d
Jens Galle <galle.jens@telenet.be>
2023-09-02 14:25:20 +0200

    fix(fuel): fucking baseevent wrapper var names are wrong fuckme

--- resources/[dg]/dg-vehicles/typescript/src/server/modules/fuel/controller.fuel.ts
+++ resources/[dg]/dg-vehicles/typescript/src/server/modules/fuel/controller.fuel.ts
@@ -1,5 +1,4 @@
-import { BaseEvents, Events } from '@dgx/server';
-import { fuelManager } from './classes/fuelManager';
+import { Events } from '@dgx/server';
 import { doRefuel, openRefuelMenu } from './service.fuel';
 
 Events.onNet('vehicles:fuel:doRefuel', (src, netId: number, usingJerryCan: boolean) => {
@@ -8,15 +7,4 @@ Events.onNet('vehicles:fuel:doRefuel', (src, netId: number, usingJerryCan: boole
 
 Events.onNet('vehicles:fuel:openRefuelMenu', (src, netId: number) => {
   openRefuelMenu(src, netId);
-});
-
-// Save fuel when player stops becoming driver
-BaseEvents.onLeftVehicle((_, vehicle, seat) => {
-  if (seat !== -1) return;
-  fuelManager.saveFuel(vehicle);
-});
-
-BaseEvents.onVehicleSeatChange((_, vehicle, __, oldSeat) => {
-  if (oldSeat !== -1) return;
-  fuelManager.saveFuel(vehicle);
 });
