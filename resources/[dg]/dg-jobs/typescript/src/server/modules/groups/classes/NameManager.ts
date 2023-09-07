b0473f4515594e1e9d913af00641b0454e13d1fe
Jens Galle <galle.jens@telenet.be>
2023-09-07 02:22:22 +0200

    feat(jobs/namemanager): seed random name by vpn item id

--- resources/[dg]/dg-jobs/typescript/src/server/modules/groups/classes/NameManager.ts
+++ resources/[dg]/dg-jobs/typescript/src/server/modules/groups/classes/NameManager.ts
@@ -19,17 +19,17 @@ class NameManager extends Util.Singleton<NameManager>() {
   }
 
   public async updatePlayerName(player: Core.Characters.Player) {
-    const hasVPN = await Inventory.doesInventoryHaveItems('player', String(player.citizenid), 'vpn'); // this version is more performant if you already have cid
+    const vpnItem = await Inventory.getFirstItemOfName('player', String(player.citizenid), 'vpn'); // this version is more performant if you already have cid
 
     // If current name is already a custom generated and we about to update to another generated then just keep old.
     // Same other way around. If current name is already charname and ply dont have vpn, just keep already existing.
     // This is done to ensure player keeps same generated name after leaving/joining
     const playerNameData = this.players.get(player.citizenid);
-    if (playerNameData && playerNameData.isVPNName === hasVPN) return;
+    if (playerNameData && playerNameData.isVPNName === !!vpnItem) return;
 
-    const name = hasVPN ? Util.generateName() : `${player.charinfo.firstname} ${player.charinfo.lastname}`;
+    const name = vpnItem ? Util.generateName(vpnItem.id) : `${player.charinfo.firstname} ${player.charinfo.lastname}`;
     this.logger.debug(`Update name for ${player.name}(${player.citizenid}) to ${name}`);
-    this.players.set(player.citizenid, { name, isVPNName: hasVPN });
+    this.players.set(player.citizenid, { name, isVPNName: !!vpnItem });
   }
 
   public getName(cid: number): string {
